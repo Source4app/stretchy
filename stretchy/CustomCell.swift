52665b6a0258d64eb0014afc0b478aa286a1a925
Milan <pmilan24@gmail.com>
2020-06-01 17:28:04 +0530

    first commit

---
+++ stretchy/CustomCell.swift
@@ -0,0 +1,27 @@
+//
+//  CustomCell.swift
+//  StretchyCell
+//
+//  Created by Gabriel Theodoropoulos.
+//  Copyright © 2018 Gabriel Theodoropoulos. All rights reserved.
+//
+
+import UIKit
+
+class CustomCell: UITableViewCell {
+
+    @IBOutlet weak var customLabel: UILabel!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
